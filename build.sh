e3a35e5a1dac437ff28c9adb99209907f6997146
Caio Oliveira <caiooliveirafarias0@gmail.com>
2015-08-20 10:24:07 -0300

    Revert "New SKernel Simple Build Script"
    This reverts commit b84b7f1dcf76083363b6e1256c2c216c23e2c85e.

--- build.sh
+++ build.sh
@@ -1,145 +1,107 @@
 #!/bin/bash
-# Base by cybojenix <anthonydking@gmail.com>
-# ReWriten by Caio Oliveira aka Caio99BR <caiooliveirafarias0@gmail.com>
-# Adapted by Suhail aka skyinfo <sh.skyinfo@gmail.com>
-# Rashed for the base of zip making
-# And the internet for filling in else where
 
-# You need to download https://github.com/TeamVee/android_prebuilt_toolchains
-# Clone in the same folder as the kernel
+# Written by Caio Oliveira aka Caio99BR <caiooliveirafarias0@gmail.com>
+# Rewritten by Suhail aka skyinfo <sh.skyinfo@gmail.com>
+# credits to the internet for filling in else where
 
-# Clean - Start
-
-cleanzip() {
-rm -rf zip-creator/*.zip
-rm -rf zip-creator/zImage
-rm -rf zip-creator/system/lib/modules/*.ko
-cleanzipcheck="Done"
-zippackagecheck=""
-adbcopycheck=""
+inchoice() {
+echo ""
+echo "Choose the device to build for!"; sleep .5
+echo "1) L5 E610 (NFC)"; sleep .5
+echo "2) L5 E612 (No NFC)"; sleep .5
+echo "3) L7 P700 (NFC)"; sleep .5
+echo "4) L7 P705 (No NFC)"; sleep .5
 }
 
-cleankernel() {
-make clean mrproper &> /dev/null
-cleankernelcheck="Done"
-buildprocesscheck=""
-target=""
-serie=""
-variant=""
-maindevicecheck=""
-}
-
-# Clean - End
-
-# Main Process - Start
-
-maindevice() {
-echo "1) L3 II Single"
-echo "2) L3 II Dual"
-echo "3) L5 NFC"
-echo "4) L5 NoNFC"
-echo "5) L7 NFC"
-echo "6) L7 NoNFC"
+devicechoice() {
 read -p "Choice: " -n 1 -s choice
 case "$choice" in
-	1 ) target="L3II-"; variant="Single"; echo "$choice - $target$variant"; make skernel_vee3_defconfig &> /dev/null; maindevicecheck="On";;
-	2 ) target="L3II-"; variant="Dual"; echo "$choice - $target$variant"; make skernel_vee3ds_defconfig &> /dev/null; maindevicecheck="On";;
-	3 ) target="L5-"; variant="Single"; echo "$choice - $target$variant"; make skernel_m4_defconfig &> /dev/null; maindevicecheck="On";;
-	4 ) target="L5NoNFC-"; variant="Single"; echo "$choice - $target$variant"; make skernel_m4_nonfc_defconfig &> /dev/null; maindevicecheck="On";;
-	5 ) target="L7-"; variant="Single"; echo "$choice - $target$variant"; make skernel_u0_defconfig &> /dev/null; maindevicecheck="On";;
-	6 ) target="L7NoNFC-"; variant="Single"; echo "$choice - $target$variant"; make skernel_u0_nonfc_defconfig &> /dev/null; maindevicecheck="On";;
-	* ) echo "$choice - This option is not valid"; sleep 2;;
+	1 ) export target="e610"; export defconfig=skernel_m4_defconfig;;
+	2 ) export target="e612"; export defconfig=skernel_m4_nonfc_defconfig;;
+	3 ) export target="p700"; export defconfig=skernel_u0_defconfig;;
+	4 ) export target="p705"; export defconfig=skernel_u0_nonfc_defconfig;;
+	* ) echo "$choice - This option is not valid"; sleep 2; devicechoice;;
 esac
+echo "$choice - $target "; sleep .5
 }
 
-maintoolchain() {
-if [ -d ../android_prebuilt_toolchains ]; then
-	echo "1) 4.7 Google GCC"
-	echo "2) 4.8 Google GCC"
-	echo "3) 4.6.4 Linaro GCC"
-	echo "3) 4.7.4 Linaro GCC"
-	echo "3) 4.8.4 Linaro GCC"
-	echo "3) 4.9.3 Linaro GCC"
-	read -p "Choice: " -n 1 -s toolchain
-	case "$toolchain" in
-		1 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.7/bin/arm-eabi-";;
-		2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-";;
-		3 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-unknown-linux-gnueabi-linaro_4.6.4-2013.05/bin/arm-unknown-linux-gnueabi-";;
-		4 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-unknown-linux-gnueabi-linaro_4.7.4-2013.12/bin/arm-unknown-linux-gnueabi-";;
-		5 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-";;
-		6 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03/bin/arm-cortex-linux-gnueabi-";;
-		* ) echo "$toolchain - This option is not valid"; sleep 2;;
-	esac
+mainprocess() {
+inchoice
+devicechoice
+echo ""
+echo "Choose the place of the toolchain"; sleep .5
+echo "Google GCC - 1) 4.7   | 2) 4.8"; sleep .5
+echo "Linaro GCC - 3) 4.6.4 | 4) 4.7.4 | 5) 4.8.4 | 6) 4.9.3"; sleep .5
+echo "or any key to Choose the place"; sleep .5
+read -p "Choice: " -n 1 -s toolchain
+case "$toolchain" in
+	1 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.7/bin/arm-eabi-";;
+	2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-";;
+	3 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-unknown-linux-gnueabi-linaro_4.6.4-2013.05/bin/arm-unknown-linux-gnueabi-";;
+	4 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-unknown-linux-gnueabi-linaro_4.7.4-2013.12/bin/arm-unknown-linux-gnueabi-";;
+	5 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-";;
+	6 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03/bin/arm-cortex-linux-gnueabi-";;
+	* ) toolchainplace;;
+esac
+echo "$CROSS_COMPILE"; sleep .5
+}
+
+toolchainplace() {
+echo ""
+echo "Please specify a location"; sleep 1
+echo "and the prefix of the chosen toolchain at the end"; sleep 1
+echo "Caio99BR says: GCC 4.6 ex. ../arm-eabi-4.6/bin/arm-eabi-"; sleep 2
+read -p "Place: " -s CROSS_COMPILE
+}
+
+kernelclean() {
+echo "Do you want to clean (make clean)?"
+read -p #Needs to be fixed
+echo "Cleaning..."
+make clean mrproper &> /dev/null
+}
+
+removelastzip() {
+rm -rf zip-creator/*.zip
+rm -rf zip-creator/kernel/zImage
+rm -rf zip-creator/system/lib/modules
+}
+
+coping() {
+mkdir -p zip-creator/system/lib/modules
+cp arch/arm/boot/zImage zip-creator/kernel
+find . -name *.ko | xargs cp -a --target-directory=zip-creator/system/lib/modules/
+}
+
+resume() {
+echo "Continuing...";
+}
+
+continuing() {
+echo -ne
+}
+
+preloop() {
+echo "Just wait"
+loop
+}
+
+loop() {
+LEND=$(date +"%s")
+LBUILDTIME=$(($LEND - $START))
+echo -ne "\r\033[K"
+echo -ne "\033[32mElapsed Time: $(($LBUILDTIME / 60)) minutes and $(($LBUILDTIME % 60)) seconds.\033[0m"
+sleep 1
+echo -ne "\r\033[K"
+looping
+}
+
+looping() {
+if [ -f zip-creator/*.zip ]; then
+	continuing
 else
-	echo "Script says: You don't have TeamVee Prebuilt Toolchains"
-	echo ""
-	echo "Script says: Please specify a location"
-	echo "Script says: and the prefix of the chosen toolchain at the end"
-	echo "Caio99BR says: GCC 4.6 ex. ../arm-eabi-4.6/bin/arm-eabi-"
-	read -p "Place: " CROSS_COMPILE
+	loop
 fi
-}
-
-# Main Process - End
-
-# Build Process - Start
-
-buildprocess() {
-START=$(date +"%s")
-make -j4
-END=$(date +"%s")
-BUILDTIME=$(($END - $START))
-if [ -f arch/arm/boot/zImage ]; then
-	buildprocesscheck="Done"
-	cleankernelcheck=""
-else
-	buildprocesscheck="Something is wrong, contact Dev!"
-fi
-}
-
-zippackage() {
-if [ "$variant" == "Dual" ]; then
-	todual
-fi
-
-if [ "$target" == "L3II-" ]; then
-	tol3ii
-fi
-
-if [ "$target" == "L7-" ]; then
-	tol7
-fi
-
-if [ "$target" == "L7NoNFC-" ]; then
-	tol7
-fi
-
-cp arch/arm/boot/zImage zip-creator
-find . -name *.ko | xargs cp -a --target-directory=zip-creator/system/lib/modules/ &> /dev/null
-
-zipfile="$customkernel-$target$variant-$version.zip"
-
-cd zip-creator
-zip -r $zipfile * -x */.gitignore* &> /dev/null
-cd ..
-
-if [ "$target" == "L7NoNFC-" ]; then
-	ofl7
-fi
-
-if [ "$target" == "L7-" ]; then
-	ofl7
-fi
-
-if [ "$target" == "L3II-" ]; then
-	ofl3ii
-fi
-
-if [ "$variant" == "Dual" ]; then
-	tosingle
-fi
-zippackagecheck="Done"
-cleanzipcheck=""
 }
 
 todual() {
@@ -147,177 +109,89 @@ sed 's/Single/Dual/' zip-creator/META-INF/com/google/android/updater-script > zi
 rm zip-creator/META-INF/com/google/android/updater-script
 mv zip-creator/META-INF/com/google/android/updater-script-temp zip-creator/META-INF/com/google/android/updater-script
 }
-
+#Needs to be fixed
 tosingle() {
 sed 's/Dual/Single/' zip-creator/META-INF/com/google/android/updater-script > zip-creator/META-INF/com/google/android/updater-script-temp
 rm zip-creator/META-INF/com/google/android/updater-script
 mv zip-creator/META-INF/com/google/android/updater-script-temp zip-creator/META-INF/com/google/android/updater-script
 }
 
-tol3ii() {
-sed 's/m4/vee3/' zip-creator/tools/kernel_flash.sh > zip-creator/tools/kernel_flash-temp.sh
-rm zip-creator/tools/kernel_flash.sh
-mv zip-creator/tools/kernel_flash-temp.sh zip-creator/tools/kernel_flash.sh
-sed 's/14/15/' zip-creator/tools/kernel_flash.sh > zip-creator/tools/kernel_flash-temp.sh
-rm zip-creator/tools/kernel_flash.sh
-mv zip-creator/tools/kernel_flash-temp.sh zip-creator/tools/kernel_flash.sh
-sed 's/L5/L3 II/' zip-creator/META-INF/com/google/android/updater-script > zip-creator/META-INF/com/google/android/updater-script-temp
-rm zip-creator/META-INF/com/google/android/updater-script
-mv zip-creator/META-INF/com/google/android/updater-script-temp zip-creator/META-INF/com/google/android/updater-script
-
+ziperror() {
+echo "The build failed so a zip won't be created"
 }
 
-ofl3ii() {
-sed 's/vee3/m4/' zip-creator/tools/kernel_flash.sh > zip-creator/tools/kernel_flash-temp.sh
-rm zip-creator/tools/kernel_flash.sh
-mv zip-creator/tools/kernel_flash-temp.sh zip-creator/tools/kernel_flash.sh
-sed 's/15/14/' zip-creator/tools/kernel_flash.sh > zip-creator/tools/kernel_flash-temp.sh
-rm zip-creator/tools/kernel_flash.sh
-mv zip-creator/tools/kernel_flash-temp.sh zip-creator/tools/kernel_flash.sh
-sed 's/L3 II/L5/' zip-creator/META-INF/com/google/android/updater-script > zip-creator/META-INF/com/google/android/updater-script-temp
-rm zip-creator/META-INF/com/google/android/updater-script
-mv zip-creator/META-INF/com/google/android/updater-script-temp zip-creator/META-INF/com/google/android/updater-script
+ziper() {
+zipfile="$custom_kernel-$target-$serie-$variant-$version.zip"
+cd zip-creator
+zip -r $zipfile * -x *kernel/.gitignore*
+cd ..
 }
 
-tol7() {
-sed 's/m4/u0/' zip-creator/tools/kernel_flash.sh > zip-creator/tools/kernel_flash-temp.sh
-rm zip-creator/tools/kernel_flash.sh
-mv zip-creator/tools/kernel_flash-temp.sh zip-creator/tools/kernel_flash.sh
-sed 's/L5/L7/' zip-creator/META-INF/com/google/android/updater-script > zip-creator/META-INF/com/google/android/updater-script-temp
-rm zip-creator/META-INF/com/google/android/updater-script
-mv zip-creator/META-INF/com/google/android/updater-script-temp zip-creator/META-INF/com/google/android/updater-script
+#Needs to be fixed
+buildprocess() {
+echo "Building..."
+sleep 1
+echo
+make $defconfig &> /dev/null
+make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+if [ -f arch/arm/boot/zImage ]; then
+	coping
+
+	if [ "$variant" == "Dual" ]; then
+		todual
+	fi
+
+	ziper
+
+	if [ "$variant" == "Dual" ]; then
+		tosingle
+	fi
+fi
 }
 
-ofl7() {
-sed 's/u0/m4/' zip-creator/tools/kernel_flash.sh > zip-creator/tools/kernel_flash-temp.sh
-rm zip-creator/tools/kernel_flash.sh
-mv zip-creator/tools/kernel_flash-temp.sh zip-creator/tools/kernel_flash.sh
-sed 's/L7/L5/' zip-creator/META-INF/com/google/android/updater-script > zip-creator/META-INF/com/google/android/updater-script-temp
-rm zip-creator/META-INF/com/google/android/updater-script
-mv zip-creator/META-INF/com/google/android/updater-script-temp zip-creator/META-INF/com/google/android/updater-script
-}
+scriptrev=8
 
-# Build Process - End
+location=.
+custom_kernel=SKernel
+version=M7
 
-# ADB - Start
-
-adbcopy() {
-echo "Script says: You want to copy to Internal or External Card?"
-echo "i) For Internal"
-echo "e) For External"
-read -p "Choice: " -n 1 -s adbcoping
-case "$adbcoping" in
-	i ) echo "Coping to Internal Card..."; adb shell rm -rf /storage/sdcard0/$zipfile; adb push zip-creator/$zipfile /storage/sdcard0/$zipfile &> /dev/null; adbcopycheck="Done";;
-	e ) echo "Coping to External Card..."; adb shell rm -rf /storage/sdcard1/$zipfile; adb push zip-creator/$zipfile /storage/sdcard1/$zipfile &> /dev/null; adbcopycheck="Done";;
-	* ) echo "$adbcoping - This option is not valid"; sleep 2;;
-esac
-}
-
-# ADB - End
-
-# Menu - Start
-
-customkernel=SKernel
-version=M6
+cd $location
 export ARCH=arm
 
-buildsh() {
-kernelversion=`cat Makefile | grep VERSION | cut -c 11- | head -1`
-kernelpatchlevel=`cat Makefile | grep PATCHLEVEL | cut -c 14- | head -1`
-kernelsublevel=`cat Makefile | grep SUBLEVEL | cut -c 12- | head -1`
-kernelname=`cat Makefile | grep NAME | cut -c 8- | head -1`
-clear
-echo "Caio99BR says: Simple $customkernel Build Script."
-echo "This is an open source script, feel free to use, edit and share it."
-echo "Linux Kernel $kernelversion.$kernelpatchlevel.$kernelsublevel - $kernelname"
-echo
-echo "Clean:"
-echo "1) Last Zip Package ($cleanzipcheck)"
-echo "2) Kernel ($cleankernelcheck)"
-echo
-echo "Main Process:"
-echo "3) Device Choice ($target$variant)"
-echo "4) Toolchain Choice ($CROSS_COMPILE)"
-echo
-echo "Build Process:"
-if ! [ "$maindevicecheck" == "" ]; then
-	if ! [ "$CROSS_COMPILE" == "" ]; then
-		echo "5) Build Kernel ($buildprocesscheck)"
-	else
-		echo "Use "4" first."
-	fi
-else
-	echo "Use "3" first."
-fi
-if [ -f arch/arm/boot/zImage ]; then
-	echo "6) Build Zip Package ($zippackagecheck)"
-fi
-if [ -f zip-creator/*.zip ]; then
-	echo
-	echo "7) Copy to device - Via Adb ($adbcopycheck)"
-fi
-if [ "$adbcopycheck" == "Done" ]; then
-	echo
-	echo "8) Reboot device to recovery"
-fi
-echo
-if ! [ "$BUILDTIME" == "" ]; then
-	echo -e "\033[32mBuild Time: $(($BUILDTIME / 60)) minutes and $(($BUILDTIME % 60)) seconds.\033[0m"
-	echo
-fi
-echo "q) Quit"
-read -n 1 -p "Choice: " -s x
-case $x in
-	1) echo "$x - Cleaning Zips..."; cleanzip; buildsh;;
-	2) echo "$x - Cleaning Kernel..."; cleankernel; buildsh;;
-	3) echo "$x - Device choice"; maindevice; buildsh;;
-	4) echo "$x - Toolchain choice"; maintoolchain; buildsh;;
-	5) if [ -f .config ]; then
-		echo "$x - Building Kernel..."; buildprocess; buildsh
-	fi;;
-	6) if [ -f arch/arm/boot/zImage ]; then
-		echo "$x - Ziping Kernel..."; zippackage; buildsh
-	fi;;
-	7) if [ -f zip-creator/*.zip ]; then
-		echo "$x - Coping Kernel..."; adbcopy; buildsh
-	fi;;
-	8) if [ "$adbcopycheck" == "Done" ]; then
-		echo "$x - Rebooting $target$variant..."; adb reboot recovery; buildsh
-	fi;;
-	q) echo "Ok, Bye!"; zippackagecheck="";;
-	*) echo "$x - This option is not valid"; sleep 2; buildsh;;
+echo ""
+echo "Caio99BR says: This is an open source script, feel free to use and share it."; sleep .5
+echo "Caio99BR says: Kernel Build Script Revision $scriptrev."; sleep .5
+
+removelastzip
+
+echo ""
+echo "Script says: Choose."
+read -p "Script says: Any key for Restart Building Process or N for Continue: " -n 1 -s clean
+case $clean in
+	n) resume; inchoice; devicechoice;;
+	N) resume; inchoice; devicechoice;;
+	*) kernelclean; mainprocess;;
 esac
-}
 
-# Menu - End
+echo ""
+echo -e "Now, building the $custom_kernel for $target $version Edition!"; sleep .5
 
-# The core of script is here!
+echo ""
+echo "You want to see the details of kernel build?"
+read -p "Enter any key for Yes or N for No: " -n 1 -t 10 -s clean
+START=$(date +"%s")
+case $clean in
+	n) buildprocess &> /dev/null | preloop;;
+	N) buildprocess &> /dev/null | preloop;;
+	*) buildprocess;;
+esac
 
-if ! [ -e build.sh ]; then
-	echo
-	echo "Ensure you run this file from the SAME folder as where it was,"
-	echo "otherwise the script will have problems running the commands."
-	echo "After you 'cd' to the correct folder, start the build script"
-	echo "with the ./build.sh command, NOT with any other command!"
-	echo; sleep 3
+if [ -f zip-creator/$zipfile ]; then
+	echo -e "\033[36mPackage Complete: zip-creator/$zipfile"
 else
-	if [ -f zip-creator/*.zip ]; then
-		cleanzipcheck=""
-	else
-		cleanzipcheck="Done"
-	fi
-
-	if [ -f .config ]; then
-		cleankernelcheck=""
-	else
-		cleankernelcheck="Done"
-	fi
-
-	if [ -f arch/arm/boot/zImage ]; then
-		buildprocesscheck="Done"
-	else
-		buildprocesscheck=""
-	fi
-
-	buildsh
+	ziperror
 fi
+
+END=$(date +"%s")
+BUILDTIME=$(($END - $START))
+echo -e "\033[32mBuild Time: $(($BUILDTIME / 60)) minutes and $(($BUILDTIME % 60)) seconds.\033[0m"
